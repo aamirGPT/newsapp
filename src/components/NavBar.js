6f776cd57df5a63b82932648d18b88f0dadafc77
Mohammed Aamir Mansoori <aamir.mansoori2911@gmail.com>
2023-12-15 12:35:21 +0530

    fetching data from the news api. Also added some pagination

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ export class NavBar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-light">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/">
                             Navbar
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,13 +4,13 @@ export class NewsItem extends Component {
     render() {
         let { title, description, imageURL, newsURL } = this.props;
         return (
-            <div className="my-3">
+            <div className="my-3 newsitembody">
                 <div className="card" style={{ width: "18rem" }}>
                     <img src={imageURL} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsURL} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">
+                        <a href={newsURL} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">
                             Read More
                         </a>
                     </div>
