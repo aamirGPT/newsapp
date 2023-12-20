ca07cf569e5571e8b7ff7c186210c87b22040b54
Mohammed Aamir Mansoori <aamir.mansoori2911@gmail.com>
2023-12-20 17:09:42 +0530

    added some more demo data

--- src/components/News.js
+++ src/components/News.js
@@ -8,7 +8,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 const News = (props) => {
     const demoData = {
         status: "ok",
-        totalResults: 36,
+        totalResults: 40,
         articles: [
             {
                 source: {
@@ -270,6 +270,306 @@ const News = (props) => {
                 publishedAt: "2023-12-12T03:45:39Z",
                 content:
                     "Comment on this story\r\nComment\r\nAdd to your saved stories\r\nSave\r\nBefore Earth had biology, it had chemistry. How the one followed from the other how a bunch of boringmolecules transformed themselves … [+6844 chars]",
+            },
+            {
+                source: {
+                    id: null,
+                    name: "Yahoo Entertainment",
+                },
+                author: "Winnie Hsu",
+                title: "European Bonds and Stocks Gain on Rate-Cut Bets: Markets Wrap - Yahoo Finance",
+                description:
+                    "(Bloomberg) -- European stocks gained and bond yields across the euro region fell on Wednesday as worsening economic data and slowing inflation underscored...",
+                url: "https://finance.yahoo.com/news/asia-stocks-track-wall-street-223104125.html",
+                urlToImage:
+                    "https://media.zenfs.com/en/bloomberg_markets_842/d78a4b655e5928e5441d8c27916d4283",
+                publishedAt: "2023-12-20T08:20:49Z",
+                content:
+                    "(Bloomberg) -- European stocks gained and bond yields across the euro region fell on Wednesday as worsening economic data and slowing inflation underscored expectations for interest-rate cuts next ye… [+5189 chars]",
+            },
+            {
+                source: {
+                    id: "reuters",
+                    name: "Reuters",
+                },
+                author: "Reuters",
+                title: "US and China remain top credit ratings to watch in 2024 - Reuters",
+                description: null,
+                url: "https://www.reuters.com/markets/us-china-remain-top-credit-ratings-watch-2024-2023-12-20/",
+                urlToImage: null,
+                publishedAt: "2023-12-20T07:34:45Z",
+                content: null,
+            },
+            {
+                source: {
+                    id: "reuters",
+                    name: "Reuters",
+                },
+                author: "Reuters",
+                title: "Israel battles Hamas on streets of Gaza city as UN delays vote again - Reuters",
+                description: null,
+                url: "https://www.reuters.com/world/middle-east/israel-battles-hamas-streets-gaza-city-un-delays-vote-again-2023-12-20/",
+                urlToImage: null,
+                publishedAt: "2023-12-20T07:30:35Z",
+                content: null,
+            },
+            {
+                source: {
+                    id: "al-jazeera-english",
+                    name: "Al Jazeera English",
+                },
+                author: "Al Jazeera",
+                title: "France passes tough immigration bill amid Macron party rebellion - Al Jazeera English",
+                description:
+                    "The new legislation includes amendments on residency and citizenship that won the approval of the far right.",
+                url: "https://www.aljazeera.com/news/2023/12/20/france-passes-tough-immigration-bill-amid-macron-party-rebellion",
+                urlToImage:
+                    "https://www.aljazeera.com/wp-content/uploads/2023/12/2023-12-19T223102Z_202529683_RC2M05AP23E7_RTRMADP_3_FRANCE-MIGRATION-1703050895.jpg?resize=1920%2C1440",
+                publishedAt: "2023-12-20T06:04:35Z",
+                content:
+                    "The French parliament has passed by a wide margin an immigration bill backed by President Emmanuel Macron after a rebellion within his party over the toughened-up legislation that had secured the end… [+2768 chars]",
+            },
+            {
+                source: {
+                    id: null,
+                    name: "[Removed]",
+                },
+                author: null,
+                title: "[Removed]",
+                description: "[Removed]",
+                url: "https://removed.com",
+                urlToImage: null,
+                publishedAt: "1970-01-01T00:00:00Z",
+                content: "[Removed]",
+            },
+            {
+                source: {
+                    id: "reuters",
+                    name: "Reuters",
+                },
+                author: "Reuters",
+                title: "FedEx profit misses, cuts full-year revenue forecast; shares sink - Reuters",
+                description: null,
+                url: "https://www.reuters.com/business/fedex-cuts-full-year-revenue-forecast-2023-12-19/",
+                urlToImage: null,
+                publishedAt: "2023-12-20T05:13:00Z",
+                content: null,
+            },
+            {
+                source: {
+                    id: null,
+                    name: "247Sports",
+                },
+                author: "Steve Wiltfong",
+                title: "Blue-Chip Buzz: The latest on top prospects as morning of the Early Signing Period hits - 247Sports",
+                description:
+                    "News and notes with the three-day Early Signing Period on the horizon.",
+                url: "https://247sports.com/Article/blue-chip-buzz-the-latest-on-top-prospects-with-less-than-eight-hours-till-signing-day-begins-223546672",
+                urlToImage:
+                    "https://s3media.247sports.com/Uploads/Assets/221/731/11731221.jpg",
+                publishedAt: "2023-12-20T04:26:40Z",
+                content:
+                    "We are under eight hours until the three-day Early Signing Period begins and college football prospects can start putting pen to paper and locking in their respective futures. There will be many rush… [+6059 chars]",
+            },
+            {
+                source: {
+                    id: "politico",
+                    name: "Politico",
+                },
+                author: null,
+                title: "Judge orders Rep. Scott Perry to disclose 1600 messages to federal prosecutors - POLITICO",
+                description:
+                    "The records could help fill crucial gaps in special counsel Jack Smith’s investigation of Trump's election interference.",
+                url: "https://www.politico.com/news/2023/12/19/scott-perry-ordered-to-disclose-messages-trump-00132623",
+                urlToImage:
+                    "https://static.politico.com/40/73/4ce3f9cb4e7c90e738b3eb0f8147/house-fbi-headquarters-06969.jpg",
+                publishedAt: "2023-12-20T03:46:05Z",
+                content:
+                    "The records in question could help fill crucial gaps in special counsel Jack Smiths investigation. An inadvertently disclosed court document obtained last month by POLITICO revealed key aspects of th… [+2928 chars]",
+            },
+            {
+                source: {
+                    id: "business-insider",
+                    name: "Business Insider",
+                },
+                author: "Amanda Goh",
+                title: "George Clooney says being on 'Friends' didn't bring Matthew Perry joy - Business Insider",
+                description:
+                    "George Clooney revealed in a Deadline interview that Matthew Perry always spoke about his dream of landing a role on a sitcom when he was younger.",
+                url: "https://www.businessinsider.com/george-clooney-says-friends-didnt-bring-matthew-perry-joy-2023-12",
+                urlToImage:
+                    "https://i.insider.com/65824b091c5c7b8c9a0a1b01?width=1200&format=jpeg",
+                publishedAt: "2023-12-20T03:46:00Z",
+                content:
+                    'George Clooney says that starring in "Friends" didn\'t bring his longtime pal Matthew Perry "joy or happiness or peace."\r\nIn a new interview with Deadline, the 62-year-old actor opened up about his fr… [+1636 chars]',
+            },
+            {
+                source: {
+                    id: "fox-news",
+                    name: "Fox News",
+                },
+                author: "Christine Rousselle",
+                title: "Minnesota commission chooses new state flag design to replace old one deemed problematic - Fox News",
+                description:
+                    "Minnesota's State Emblems Redesign Commission voted 11-1 to approve a new flag design, which, pending legislative approval, will begin flying in May 2024.",
+                url: "https://www.foxnews.com/lifestyle/minnesota-commission-chooses-new-state-flag-design-replace-old-deemed-problematic",
+                urlToImage:
+                    "https://static.foxnews.com/foxnews.com/content/uploads/2023/12/AP23353664697824.jpg",
+                publishedAt: "2023-12-20T03:37:00Z",
+                content:
+                    "A Minnesota commission tasked with redesigning the state's flag and seal announced its selection for the updated state flag on Dec. 19. \r\nThe State Emblems Redesign Commission voted 11-1 to approve t… [+4358 chars]",
+            },
+            {
+                source: {
+                    id: "reuters",
+                    name: "Reuters",
+                },
+                author: "Reuters",
+                title: "Trump barred from Colorado primary ballot for role in US Capitol attack - Reuters",
+                description: null,
+                url: "https://www.reuters.com/legal/colorado-supreme-court-disqualifies-trump-holding-office-filing-2023-12-19/",
+                urlToImage: null,
+                publishedAt: "2023-12-20T03:27:00Z",
+                content: null,
+            },
+            {
+                source: {
+                    id: "associated-press",
+                    name: "Associated Press",
+                },
+                author: "HANNAH FINGERHUT, ALI SWENSON",
+                title: "Trump defends controversial immigrant remarks at Iowa rally - The Associated Press",
+                description:
+                    "Former President Donald Trump is defending his comments about migrants crossing the southern border, who he had said are poisoning the blood of America. He said at a campaign rally in Waterloo, Iowa, on Tuesday that he had not read “Mein Kampf,” referencing A…",
+                url: "https://apnews.com/article/donald-trump-immigration-iowa-dff7f632948fa6511fb7d1955a28610c",
+                urlToImage:
+                    "https://dims.apnews.com/dims4/default/1abe429/2147483647/strip/true/crop/5386x3030+0+281/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F77%2F78%2F532f568a04d685fd80d9f94d5c6c%2F85950d13fb734af8982238decd77ca28",
+                publishedAt: "2023-12-20T02:48:45Z",
+                content:
+                    "WATERLOO, Iowa (AP) Former President Donald Trump on Tuesday defended his comments about migrants crossing the southern border poisoning the blood of America, and he reinforced the message while deny… [+6484 chars]",
+            },
+            {
+                source: {
+                    id: null,
+                    name: "The Times of Israel",
+                },
+                author: "The Times of Israel",
+                title: "IDF probe said to find troops who killed hostages not briefed on Hebrew sign in area - The Times of Israel",
+                description: null,
+                url: "https://www.timesofisrael.com/idf-probe-said-to-find-troops-who-killed-hostages-not-briefed-on-hebrew-sign-in-area/",
+                urlToImage: null,
+                publishedAt: "2023-12-20T02:24:37Z",
+                content: null,
+            },
+            {
+                source: {
+                    id: "the-hill",
+                    name: "The Hill",
+                },
+                author: "Tara Suter",
+                title: "Stephen A. Smith slams Texas immigration law as 'a disgrace,' 'inhumane' - The Hill",
+                description:
+                    "Sports commentator Stephen A. Smith slammed Texas’ new immigration law in a post on his account on X, the platform formerly known as Twitter, on Tuesday. “This is a disgrace. It is inhumane,” the ESPN host said in the post.  The post also featured a clip from…",
+                url: "https://thehill.com/homenews/media/4368906-stephen-a-smith-slams-texas-immigration-law/",
+                urlToImage:
+                    "https://thehill.com/wp-content/uploads/sites/2/2023/04/StephenASmithSemafor_Getty.jpg?w=1280",
+                publishedAt: "2023-12-20T02:18:00Z",
+                content:
+                    "Skip to content\r\nSports commentator Stephen A. Smith slammed Texas’ new immigration law in a post on his account on X, the platform formerly known as Twitter, on Tuesday.\r\n“This is a disgrace. It is … [+2256 chars]",
+            },
+            {
+                source: {
+                    id: null,
+                    name: "The Athletic",
+                },
+                author: "Kelly Iko",
+                title: "Ja Morant makes season debut, starts vs. Pelicans: What to know about his return from suspension - The Athletic",
+                description:
+                    "Morant was suspended for the first 25 games of the season for multiple instances of brandishing a gun in public.",
+                url: "https://theathletic.com/5147182/2023/12/19/ja-morant-nba-grizzlies-return-suspension/",
+                urlToImage:
+                    "https://cdn.theathletic.com/app/uploads/2023/12/19201229/GettyImages-1868428512-scaled.jpg",
+                publishedAt: "2023-12-20T02:02:29Z",
+                content:
+                    "NEW ORLEANS A few moments before Tuesday nights GrizzliesPelicans matchup, which marked Ja Morants first time back on an NBA court in more than seven months, the 24-year-old All-Star guard leaned aga… [+9857 chars]",
+            },
+            {
+                source: {
+                    id: null,
+                    name: "YouTube",
+                },
+                author: null,
+                title: "Iceland volcano erupts in country's most populated area - FOX 11 Los Angeles",
+                description: null,
+                url: "https://www.youtube.com/watch?v=UEoCgSib0hU",
+                urlToImage: null,
+                publishedAt: "2023-12-20T01:57:22Z",
+                content:
+                    "Your browser isnt supported anymore. Update it to get the best YouTube experience and our latest features. Learn more\r\nRemind me later",
+            },
+            {
+                source: {
+                    id: null,
+                    name: "ScienceAlert",
+                },
+                author: "Business Insider",
+                title: "Hold Onto Your Hats: JWST Unveils Unprecedentedly Crisp View of Uranus And Its Rings - ScienceAlert",
+                description:
+                    "NASA's James Webb Space Telescope has done it again.",
+                url: "https://www.sciencealert.com/hold-onto-your-hats-jwst-unveils-unprecedentedly-crisp-view-of-uranus-and-its-rings",
+                urlToImage:
+                    "https://www.sciencealert.com/images/2023/12/jwst-uranus-20-dec-32.jpeg",
+                publishedAt: "2023-12-20T01:27:02Z",
+                content:
+                    "NASA's James Webb Space Telescope has done it again.\r\nAn enhanced image from the high-powered telescope orbiting in space shows the planet Uranus in stunning new detail.\r\nThe photo captures the icy b… [+2250 chars]",
+            },
+            {
+                source: {
+                    id: "cbs-news",
+                    name: "CBS News",
+                },
+                author: "Natalie Duddridge",
+                title: "N.Y. Gov. Kathy Hochul signs controversial legislation to create slavery reparations commission - CBS New York",
+                description:
+                    "Hochul said she knows many New York residents do not support it, but added it had to be signed.",
+                url: "https://www.cbsnews.com/newyork/news/gov-kathy-hochul-signs-controversial-legislation-to-create-slavery-reparations-commission/",
+                urlToImage:
+                    "https://assets2.cbsnewsstatic.com/hub/i/r/2023/12/19/023fe058-c3dc-477b-ae47-f404481203b6/thumbnail/1200x630/f6ae5c6ed47c977997103c6b862bb9c6/duddridge-12-image-720.jpg?v=5382e209c94ee904b3a96a69f8ca0ce0",
+                publishedAt: "2023-12-20T01:26:00Z",
+                content:
+                    "NEW YORK -- Gov. Kathy Hochul signed historic racial justice legislation on Tuesday, creating a committee to consider reparations for slavery.\r\nThe new law authorizes the creation of a community comm… [+2845 chars]",
+            },
+            {
+                source: {
+                    id: "ign",
+                    name: "IGN",
+                },
+                author: "Alex Stedman",
+                title: "Suicide Squad: Kill the Justice League's Epic Games Store Release Delayed to March - IGN",
+                description:
+                    "The Epic Games Store release for Suicide Squad: Kill the Justice League has been delayed to March, while all other storefronts remain on track for Feb. 2.",
+                url: "https://www.ign.com/articles/suicide-squad-kill-the-justice-leagues-epic-games-store-release-delayed-to-march",
+                urlToImage:
+                    "https://assets-prd.ignimgs.com/2023/12/08/suicide-squad-1702002302912.jpg?width=1280",
+                publishedAt: "2023-12-20T00:50:36Z",
+                content:
+                    "Suicide Squad: Kill the Justice League has been delayed on just one storefront: Epic Games Store.\r\nThe highly anticipated Arkham game will now debut on Epic Games Store on March 5, 2024, a community … [+1569 chars]",
+            },
+            {
+                source: {
+                    id: "cbs-news",
+                    name: "CBS News",
+                },
+                author: "Tucker Reals",
+                title: "Who are the Houthi rebels? What to know about the Yemeni militants attacking ships in the Red Sea - CBS News",
+                description:
+                    "What to know about the Houthi rebel movement, an Iran-backed group that controls much of Yemen, as it attacks ships in the Red Sea.",
+                url: "https://www.cbsnews.com/news/who-are-houthi-rebels-what-to-know-yemen-militants-attack-ships-red-sea/",
+                urlToImage:
+                    "https://assets2.cbsnewsstatic.com/hub/i/r/2022/10/06/b0373bc4-407d-4daa-b26f-29d89b429d2b/thumbnail/1200x630/b43c8e29cc07323536553d9a03dd5354/houthi-yemen-ap22278622673883.jpg?v=5382e209c94ee904b3a96a69f8ca0ce0",
+                publishedAt: "2023-12-20T00:42:00Z",
+                content:
+                    "For weeks, Houthi rebels in Yemen have launched drone and rocket attacks targeting ships in the Red Sea and the Bab al-Mandab Strait, a strategic passage that connects the Red Sea and the Arabian Sea… [+6903 chars]",
             },
         ],
     };
